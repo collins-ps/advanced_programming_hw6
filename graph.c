9e5d48651f1a75176dc58eb4634897234d858027
collins-ps <90463501+collins-ps@users.noreply.github.com>
2021-12-07 19:07:43 -0600

    Update graph.c

--- graph.c
+++ graph.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <assert.h>
+#include <time.h>
 #include "graph.h"
 
 void print_graph(Graph *g){
@@ -218,13 +219,170 @@ int find_period(Graph *g){
     return gcd;
 }
 
+double **generateMatrix(Graph *g){
+  double **matrix = malloc( (g->nvertices) * sizeof(double *));
+  for (int i = 1; i < g->nvertices; i++){
+    matrix[i] = malloc(g->nvertices * sizeof(double));
+    Edgenode *edge =  g->edges[i];
+    for (int j = 1; j < g->nvertices; j++) {
+      if (edge == NULL || edge->y != j){
+        matrix[i][j] = 0;
+      }
+      else{
+        matrix[i][edge->y] = edge->weight;
+        edge = edge->next;
+      }
+    }
+  }
+  return matrix;
+}
+
+void destroyMatrix(double **matrix){
+  for (int i = 1; i < 101; i++){
+    free(matrix[i]);
+  }
+  free(matrix);
+}
+
+void printMatrix(double **matrix){
+  for (int i = 1; i < 101; i++){
+    for (int j = 1; j < 101; j++) {
+      printf("%.9f ", matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void mulMat(double **mat1, double **mat2, double **rslt) { // referenced https://www.geeksforgeeks.org/c-program-multiply-two-matrices/
+    // double **rslt = malloc( (101) * sizeof(double *));
+    // for (int i = 1; i < 101; i++)
+    //   rslt[i] = malloc(101 * sizeof(double));
+ 
+    for (int i = 1; i < 101; i++) {
+        for (int j = 1; j < 101; j++) {
+            rslt[i][j] = 0;
+ 
+            for (int k = 1; k < 101; k++) {
+                rslt[i][j] += mat1[i][k] * mat2[k][j];
+            }
+ 
+            // printf("%d\t", rslt[i][j]);
+        }
+ 
+        // printf("\n");
+    }
+    // return rslt;
+}
+
+double **generateCDF(double **pdf){
+  double **matrix = malloc( (101) * sizeof(double *));
+  for (int i = 1; i < 101; i++){
+    matrix[i] = malloc(101 * sizeof(double));
+    matrix[i][1] = pdf[i][1];
+    for (int j = 2; j < 100; j++){
+      matrix[i][j] = pdf[i][j] + matrix[i][j-1];
+    }
+    matrix[i][100] = 1; // to renormalize matrix due to rounding errors
+  }
+  return(matrix);
+}
+
+void markovChain(int x, double **pdf, int nsteps, double *visits){
+  double **cdf = generateCDF(pdf);
+  // int visits[101]; 
+  // for (int i = 1; i < 101; i++){
+  //   visits[i] = 0;
+  // }
+  for (int i = 1; i <= nsteps; i++){
+    double rn = (double)rand() / (double)RAND_MAX;
+    int j = 1;
+    double cutoff = cdf[x][j];
+    while (rn > cutoff){
+      j++;
+      cutoff = cdf[x][j];
+    }
+    x = j;
+    visits[j]++;
+  }
+  destroyMatrix(cdf);
+  // for (int i = 1; i < 101; i++){
+  //   printf("%.3f ", (double)visits[i] / (double)nsteps );
+  // }
+}
+
+
 int main(){
+    srand(time(NULL)); 
     Graph g;
     read_graph("markov-graph.txt",&g,true); 
-    if (kosaraju(&g) == 1)
-        printf("Graph is irreducible.\n");
-    printf("Period is: %d\n", find_period(&g));
+    // if (kosaraju(&g) == 1)
+    //     printf("Graph is irreducible.\n");
+    // printf("Period is: %d\n", find_period(&g));
+    double **matrix = generateMatrix(&g);
+
+    // double **cdf = generateCDF(matrix);
+    // printMatrix(cdf);
+    // destroyMatrix(cdf);
+
+    double *visits = malloc(sizeof(double) * 101);
+    int num_iters = 10; // in millions
+    for (int i = 1; i < 101; i++){
+      visits[i] = 0;
+    }
+    for (int i = 1; i <= num_iters; i++){
+      int r = rand() % 101;
+      markovChain(r,matrix,1000000, visits);
+    }
+    double sum = 0;
+    for (int i = 1; i < 101; i++){
+      visits[i] = (double)visits[i] / (double)(num_iters * 1000000);
+      printf("%.3f ", visits[i]);
+      sum += visits[i];
+    }
+    assert(sum - 1 < 0.000001);
+    
+    free(visits);
+
+    // printMatrix(matrix);
+    // findProbabilities(matrix, &g);
+    destroyMatrix(matrix);
+    
     destroyGraph(&g);
     return 0;
 }
 
+// double **findProbabilities(double **matrix, Graph *g){
+//   double **k_step = malloc( (101) * sizeof(double *));
+//   for (int i = 1; i < 101; i++){
+//     k_step[i] = malloc(101 * sizeof(double));
+//     for (int j = 1; j < 101; j++){
+//       k_step[i][j] = matrix[i][j];
+//     }
+//   }
+
+//   int steps = 10000000;
+    
+//   for (int i = 1; i <= 10000000; i++){ // 2 step
+//     mulMat(k_step, matrix, k_step);
+//   }
+//   while (k_step[1][1] - k_step[100][1] > 0.00000001){
+//     printf("Not equal\n");
+//     steps += 1000000;
+//     for (int i = 1; i <= 1000000; i++){ // 2 step
+//       mulMat(k_step, matrix, k_step);
+//     }
+//   }
+
+  
+//   // while ((k_step[1][5] - k_step[100][5] > 0.000001) ){ 
+//   //   steps++;
+//   //   mulMat(k_step, matrix, k_step);
+//   // }
+
+//   // double **k_step = mulMat(matrix, matrix);
+//   printMatrix(k_step, g);
+//   printf("\nSteps: %d\n", steps);
+//   for (int i = 1; i < 101; i++)
+//     free(k_step[i]);
+//   free(k_step);
+// }
